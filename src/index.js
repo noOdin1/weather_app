163367908f36ba2381fc5cf19669df45253470ed
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-07 17:24:39 +0800

    Added simple conversion functions, 4 total

--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@ import "./style.css";
 
 (() => {
   console.log("Weather App");
+
+  // convert Celcius to Fahrenheit
+  const cTof = (cel) => {
+    return c * (9 / 5) + 32;
+  };
+
+  // convert Fahrenheit to Celcius
+  const fToc = (fah) => {
+    return (f - 32) * (5 / 9);
+  };
+
+  // convert mph to kmh
+  const mphTokmh = (mph) => {
+    return mph * 1.609344;
+  };
+
+  // convert mph to kmh
+  const mphTokmh = (kmh) => {
+    return kmh * 0.6213711922;
+  };
 })();
